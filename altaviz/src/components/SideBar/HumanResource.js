c2bec9ad475b5eb9f0551c6de7d2b5eee0cbd52d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-31 22:52:40 +0100

    new humanresource.js: human resource section

---
+++ altaviz/src/components/SideBar/HumanResource.js
@@ -0,0 +1,16 @@
+import useFetch from "../../hooks/useFetch";
+
+function HumanResource() {
+	const { data, loading, error } = useFetch('http://localhost:8000');
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error}</p>;
+	console.log(data);
+
+	return (
+		<>
+			<h2>Human Resource Page</h2>
+		</>
+	);
+}
+export default HumanResource;
