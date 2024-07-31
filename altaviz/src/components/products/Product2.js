f6203963f7efb2759cbefcb343f7106d89867e55
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-31 22:48:47 +0100

    new product2.js: created test component

---
+++ altaviz/src/components/products/Product2.js
@@ -0,0 +1,16 @@
+import useFetch from "../../hooks/useFetch";
+
+function Product2() {
+	const { data, loading, error } = useFetch('http://localhost:8000');
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error}</p>;
+	console.log(data);
+
+	return (
+		<>
+			<h2>Product 2</h2>
+		</>
+	);
+}
+export default Product2;
