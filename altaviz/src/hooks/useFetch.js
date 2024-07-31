064f7d99638639f61c1f6f8ee6939ebdb9bcc6a2
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-31 11:52:55 +0100

    new useFetch.js: API function that consumes the backend endpoints

--- altaviz/src/hooks/useFetch.js
+++ altaviz/src/hooks/useFetch.js
@@ -10,7 +10,7 @@ function useFetch(url) {
 		try {
 			const response = await fetch(url);
 			if (!response.ok) {
-			throw new Error("Network response was not ok");
+			throw new Error("could not connect");
 			}
 			const data = await response.json();
 			setData(data);
