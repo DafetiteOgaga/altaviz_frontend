073c62936a91e4c15bd4e481968cbab5ed603fd4
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-31 11:55:48 +0100

    fix - app.js: fixed display and component rendering problems arising from navigations for header and sidebar

--- altaviz/src/App.js
+++ altaviz/src/App.js
@@ -1,13 +1,60 @@
 // import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import Testfetchapi from './components/Testfetchapi';
+// import Testfetchapi from './components/Testfetchapi';
+import SideBar from './components/SideBar/SideBar';
+import Header from './components/header/Header';
+import AppRoutes from './components/Routes/Routes';
+// import Home from './components/home/Home';
 
 function App() {
   return (
     <>
-      <h1>response from backend:</h1>
-      <Testfetchapi/>
+      <div>
+        <Router>
+          <Header />
+            <div className="sub-root">
+              <SideBar />
+              <main>
+                <AppRoutes/>
+              </main>
+            </div>
+        </Router>
+      </div>
+      <footer>footer</footer>
     </>
+
+
+
+
+
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+		// 		<p>
+		// 			<a
+		// 				className="App-link"					
+    //       	href="https://github.com/DafetiteOgaga/custom_commands"					
+    //       	target="_blank"
+		// 				rel="noopener noreferrer"
+		// 			>
+    //       	Install more custom commands
+		// 			</a>
+		// 		</p>
+    //   </header>
+    // </div>
   );
 }
 
