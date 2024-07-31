71a46fdeae0e5edf058fa848646ec39c2d28525a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-31 11:11:48 +0100

    new about component

---
+++ altaviz/src/components/home/About.js
@@ -0,0 +1,16 @@
+import useFetch from "../../hooks/useFetch";
+
+function About() {
+	const { data, loading, error } = useFetch('http://localhost:8000');
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error}</p>;
+	console.log(data);
+
+	return (
+		<>
+			<h2>About Page</h2>
+		</>
+	);
+}
+export default About;
