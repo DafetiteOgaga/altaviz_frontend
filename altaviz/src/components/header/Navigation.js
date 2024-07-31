91a60227d7117da4ef4ea337fe70b2ae64bdb996
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-07-31 22:57:24 +0100

    changes navigation.js: added links to the newly added components

--- altaviz/src/components/header/Navigation.js
+++ altaviz/src/components/header/Navigation.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import DropdownMenu from './DropdownMenu';
 
 function Navigation() {
   return (
-    <nav>
+    <nav className='nav-h1'>
       <ul className='header-ul'>
         <li><Link to="/">Home</Link></li>
+        <li><DropdownMenu /></li>
+        <li><Link to="/products">Products</Link></li> {/* redundant, so remove */}
         <li><Link to="/about">About</Link></li>
-        {/* <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li> */}
+        <li><Link to="/contact">Contact Us</Link></li>
+        <li><Link to="/test">Test bkend</Link></li>
       </ul>
     </nav>
   );
