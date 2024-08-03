699935c2183b07741d1fd82e01a46f97b86b1d77
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-03 13:54:59 +0100

    fix helpdesk.js: fixed relative path to usefetch hook

--- altaviz/src/components/SideBar/HelpDesk.js
+++ altaviz/src/components/SideBar/HelpDesk.js
@@ -1,4 +1,4 @@
-import useFetch from "../../hooks/useFetch";
+import useFetch from "../hooks/useFetch";
 
 function HelpDesk() {
 	const { data, loading, error } = useFetch('http://localhost:8000');
