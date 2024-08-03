5d183b5f961c32d7c32b5f5a1e28d305eca4c801
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-03 14:03:38 +0100

    fix home.js: implemented card views and used an object as its props

--- altaviz/src/components/home/Home.js
+++ altaviz/src/components/home/Home.js
@@ -1,21 +1,45 @@
-import useFetch from "../../hooks/useFetch";
+// import useFetch from "../../hooks/useFetch";
 import Hero from "./Hero";
 import AboutAtms from "./AboutAtms";
-import PorductCards from "./ProductCards";
+import ProductCards from "./ProductCards";
+// import cardreader from "./product_images/Card Reader.png"
 
 function Home() {
-	const { data, loading, error } = useFetch('http://localhost:8000');
+	// const { data, loading, error } = useFetch('http://localhost:8000');
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error: {error}</p>;
-	console.log(data);
+	// if (loading) return <p>Loading...</p>;
+	// if (error) return <p>Error: {error}</p>;
+	// console.log(data);
 
+	function getImages(r) {
+		return r.keys().map(r);
+	}
+	const images = getImages(require.context('./product_images/', false, /\.(png|jpe?g|svg)$/));
+	const titles = [
+		'H22V series',
+		'H68NL Series Intelligent Cash Recycler',
+		'grg-200-v-sorting-machine',
+		'H34 series',
+	]
+	const descriptions = [
+		'description1',
+		'description2',
+		'description3',
+		'description4',
+	]
+	const cardData = images.map((image, index) => {
+		console.log('index', index, 'title', titles[index] , 'descriptions', descriptions[index], 'image', image)
+		return ({
+			title: titles[index],
+            description: descriptions[index],
+            image: image,
+		})});
+	console.log('cardData:', cardData);
 	return (
 		<>
 		    <Hero />
 			<AboutAtms />
-			<PorductCards />
-			<h2>Home Page</h2>
+			<ProductCards cardData={cardData} />
 		</>
 	);
 }
