bbd29063e7e39882ee13adeb0709daacce43abcd
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-04 15:22:48 +0100

    updated

--- altaviz/src/components/products/Product1.js
+++ altaviz/src/components/products/Product1.js
@@ -1,15 +1,17 @@
-import useFetch from "../hooks/useFetch";
+// import useFetch from "../hooks/useFetch";
 
 function Product1() {
-	const { data, loading, error } = useFetch('http://localhost:8000');
+	// const { data, loading, error } = useFetch('http://localhost:8000');
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error: {error}</p>;
-	console.log(data);
+	// if (loading) return <p>Loading...</p>;
+	// if (error) return <p>Error: {error}</p>;
+	// console.log(data);
 
 	return (
 		<>
-			<h2>Product 1</h2>
+			<div className="background-color">
+				<h2>Product k1</h2>
+			</div>
 		</>
 	);
 }
