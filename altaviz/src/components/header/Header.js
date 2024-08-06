d5caf94a4176df1f123b28543c190c2d4ac7ef70
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-06 12:47:14 +0100

    fix for Header.js: fixed navigation using anchor and used the react usenavigate instead

--- altaviz/src/components/header/Header.js
+++ altaviz/src/components/header/Header.js
@@ -1,13 +1,21 @@
 // import useFetch from '../hooks/useFetch';
 import logo from '../../logo/altaviz_logo.png';
 import Navigation from './Navigation';
+import { useNavigate } from 'react-router-dom';
 import "./header.css"
 
 function Header() {
+	const navigateTo = useNavigate();
 	return (
 		<header>
 			<div className='logo-nav'>
-				<a href='/'>
+				<a
+				href='/'
+				onClick={(e) => {
+					e.preventDefault();
+					navigateTo('/');
+				}}
+				>
 					<img src={logo} alt='alterviz logo' />
 				</a>
 				<Navigation />
