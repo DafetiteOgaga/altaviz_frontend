b68cddf51929eb575711a2b6f7bc66ac4a179673
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-08 05:48:19 +0100

    fix ContactUs.js: implemented the useFetchPost api function to send data to the backend and fixed the form elements with best practices

--- altaviz/src/components/main/ContactUs.js
+++ altaviz/src/components/main/ContactUs.js
@@ -2,6 +2,7 @@ import { useState, useContext } from 'react';
 // import useButton from '../hooks/useButton';
 import { GlobalContext } from '../context/Context';
 // import useFetchPost from '../hooks/useFetchPost';
+import "./contactus.css"
 
 const Contact = () => {
 	const fields = {
@@ -20,12 +21,17 @@ const Contact = () => {
 
 	const [isFormValid, setIsFormValid] = useState(false);
 	const [submitTrigger, setSubmitTrigger] = useState(false);
-    useFetchPost('http://localhost:8000', formData, submitTrigger);
-	// const { data, loading, error } = useFetchPost('http://localhost:8000', formData, submitTrigger);
-	// console.log('data:', data);
-	// console.log('loading:', loading);
-	// console.log('error:', error);
+	// const [ IsSubmitted, setIsSubmitted] = useState(false);
+    useFetchPost('http://localhost:8000/contact-us/', formData, submitTrigger, fields);
+	// const { data, loading, error } = useFetchPost('http://localhost:8000/contact-us/', formData, submitTrigger);
+	// console.log('response data:', data);
+	// console.log('response loading:', loading);
+	// console.log('response error:', error);
 
+	// useEffect(() => {
+	// 	setFormData(fields);
+	// 	// setIsSubmitted(false);
+	// }, [IsSubmitted])
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		// console.log(name, value);
@@ -69,65 +75,51 @@ const Contact = () => {
 		if (validateForm()) {
 			console.log('Form submitted:', formData);
 			setSubmitTrigger(true);
-			setFormData(fields)
-
-		// form submission logic
-		// const postData = async () => {
-		// 	try {
-		// 		const response = await fetch('https://example.com/api/contact', {
-		// 		method: 'POST',
-		// 		headers: {
-		// 			'Content-Type': 'application/json',
-		// 		},
-		// 		body: JSON.stringify(formData),
-		// 		});
-		// 		if (response.ok) {
-		// 		// Handle successful form submission
-		// 		alert('Form submitted successfully');
-		// 		} else {
-		// 		// Handle errors during form submission
-		// 		alert('Failed to submit the form');
-		// 		}
-		// 	} catch (error) {
-		// 		console.error('Error submitting the form:', error);
-		// 	}
-		// }
-	};};
+			// setIsSubmitted(!IsSubmitted);
+		};
+	};
 
 	return (
 		<div className='background-color contact-page'>
 			<h2>Contact Us</h2>
 			<form className='contact-form-page'
-				onSubmit={useHandleSubmit}>
+				onSubmit={useHandleSubmit}
+				onClick={useHandleSubmit}>
 				<div className='element-container'>
-				<input
-					type="text"
-					name="name"
-					placeholder='Name'
-					value={formData.name}
-					onChange={handleChange}
-				/>
-				{errors.name && <span>{errors.name}</span>}
+					<input
+						type="text"
+						name="name"
+						placeholder='Name (Optional)'
+						value={formData.name}
+						onChange={handleChange}
+					/>
+					<div className='span-container'>
+						{errors.name && <span>{errors.name}</span>}
+					</div>
 				</div>
 				<div className='element-container'>
-				<input
-					type="email"
-					name="email"
-					placeholder='Email address'
-					value={formData.email}
-					onChange={handleChange}
-				/>
-				{errors.email && <span>{errors.email}</span>}
+					<input
+						type="email"
+						name="email"
+						placeholder='Email address'
+						value={formData.email}
+						onChange={handleChange}
+					/>
+					<div className='span-container'>
+						{errors.email && <span>{errors.email}</span>}
+					</div>
 				</div>
 				<div className='element-container'>
-				<textarea
-					name="message"
-					placeholder='Message ...'
-					value={formData.message}
-					rows='6'
-					onChange={handleChange}
-				/>
-				{errors.message && <span>{errors.message}</span>}
+					<textarea
+						name="message"
+						placeholder='Message ...'
+						value={formData.message}
+						rows='6'
+						onChange={handleChange}
+					/>
+					<div className='span-container'>
+						{errors.message && <span>{errors.message}</span>}
+					</div>
 				</div>
 				{useButton(['submit', isFormValid])}
 				{/* {loading && <p>Loading...</p>} */}
