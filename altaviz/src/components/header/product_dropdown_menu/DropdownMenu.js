972ee2b6390ee9e2594f1eebdbd4aebcca60abed
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-12 14:42:49 +0100

    new DropdownMenu.js: moved dropdown component into a separate dir for the sake of modularization

---
+++ altaviz/src/components/header/product_dropdown_menu/DropdownMenu.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './DropdownMenu.css'
+import GetProductDetails from '../../hooks/GetProductNHeroDetails';
+
+function DropdownMenu () {
+  const products = GetProductDetails();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+    const aboutIndexDisabled = document.getElementById('about-animation');
+    // const aboutIndexEnabled = document.getElementById('abot-animation');
+    if (!isOpen && aboutIndexDisabled) {
+      aboutIndexDisabled.setAttribute('id', 'abot-animation');
+      console.log('id removed');
+      console.log('about index:', aboutIndexDisabled);
+    }
+  };
+
+  return (
+    <div className="dropdown">
+      <a href='/products' onMouseOver={toggleDropdown} onClick={toggleDropdown} className="dropdown-button">
+        Products
+      </a>
+      {(isOpen) && (
+        <div className="dropdown-content">
+          {products.map(product => {
+            // console.log('Product:', product)
+            return (
+            <div key={product.id}>
+              <Link to={`/products/product/${product.id}`}>{product.description.title}</Link>
+            </div>
+          )})}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DropdownMenu;
