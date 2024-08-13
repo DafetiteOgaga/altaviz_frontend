2eaa858d7d21d4aeb501fe6eb3e1d0fc57753a9b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-13 23:46:23 +0100

    fix HumanResource.js: added buttons to add parts and components

--- altaviz/src/components/SideBar/human_resource/HumanResource.js
+++ altaviz/src/components/SideBar/human_resource/HumanResource.js
@@ -4,27 +4,25 @@ import CreateUser from "./createUser";
 import UpdateUser from "./UpdateUser";
 import { useState } from "react";
 import "./humanResource.css"
+import { useNavigate } from "react-router-dom";
 
 function HumanResource() {
+	const navigate = useNavigate();
 	const [createUser, setCreateUser] = useState(false);
 	const [updateUser, setUpdateUser] = useState(false);
+	const [partsComps, setPartsComps] = useState(false);
 	const toggleCreateUser = () => {
 		setCreateUser(!createUser);
 	}
 	const toggleUpdateUser = () => {
 		setUpdateUser(!updateUser);
 	}
-	// useEffect(() => {
-
-	// },[])
-	// const toggleCreateUser = () => {
-	// 	if (createUser) {
-	// 		setCreateUser(!createUser);
-	// 	}
-	// 	if (updateUser) {
-	// 		setUpdateUser(!updateUser);
-	// 	}
-    // };
+	const toggleAddPartsComps = () => {
+		setPartsComps(!partsComps);
+	}
+	const gotoInventory = () => {
+		navigate('/inventory');
+	}
 	const name = 'human resource';
 	const childList = [
 		['Add parts', 'part', 'Add Part', 'part-qty', 'Add Part'],
@@ -35,12 +33,21 @@ function HumanResource() {
 			<div className="background-color custodian-page">
 				<Dashboard name={name} pageName={`/${name}`} />
 				<hr style={{width: '80%'}} />
-				<div className="user-account">
+				<div className="custum-button">
+					<h5 onClick={toggleAddPartsComps}>{partsComps ? 'Close Form' : 'Add Parts/Components'}</h5>
+					<h5 onClick={gotoInventory}>Inventory</h5>
+				</div>
+				<div className="custum-button">
 					<h5 onClick={toggleCreateUser}>{createUser ? 'Close Account Creation Form' : 'Create New Account'}</h5>
 					<h5 onClick={toggleUpdateUser}>{updateUser ? 'Close Update Form' : 'Update Existing User'}</h5>
 				</div>
 				<hr style={{width: '50%'}} />
-				<LogFault childList={childList} />
+				{partsComps && (
+					<>
+						<hr />
+						<LogFault childList={childList} />
+					</>
+				)}
 				{createUser && (
 					<>
 						<hr />
