10b896c13b98a421b408f29d4e40b1f752c0c510
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-15 07:00:05 +0100

    fix refactored most part of the component to display information based on the page

--- altaviz/src/components/SideBar/log_fault/LogFault.js
+++ altaviz/src/components/SideBar/log_fault/LogFault.js
@@ -106,168 +106,196 @@ function LogFault({childList}) {
 			<div className="dash-form">
 				{mapList.map((item, mainIndex) => {
 				return (<>
-					<div>
-						{/* for custodian */}
-						{(componentPage === '/custodian') &&
-						(<h4>Having difficulty with your machine?</h4>)}
-						{/* for workshop */}
-						{(componentPage === '/workshop') &&
-						(<h4>{item[0]}</h4>)}
-					</div>
-					<div>
-						{/* for custodian */}
-						{(componentPage === '/custodian') &&
-						(<div className="to-form">
-							<h4>Log a Fault</h4>
-						</div>)}
-						<hr />
-						<form onSubmit={handleSubmit}>
-							<div>
-								{/* for custodian */}
-								{(componentPage === '/custodian') &&
-								(<div className="cust-row">
-									<div className="input-field">
-										<label htmlFor="state">State:</label>
-										<input
-										type="text"
-										name="state"
-										id="state"
-										value={formValues.state}
-										onChange={handleInputChange}
-										// readOnly={!isEditable}
-										readOnly={(componentPage === pageName && !isEditable) ? true : false}
-										/>
-										{errors.state && <span className="error">{errors.state}</span>}
-									</div>
-									<div className="input-field">
-										<label htmlFor="branch">Branch:</label>
-										<input
-										type="text"
-										name="branch"
-										id="branch"
-										value={formValues.branch}
-										onChange={handleInputChange}
-										// readOnly={!isEditable}
-										readOnly={(componentPage === pageName && !isEditable) ? true : false}
-										/>
-										{errors.branch && <span className="error">{errors.branch}</span>}
-									</div>
-								</div>)}
-
-								<div className="cust-row">
-									<div className="dynamic-cust-row">
-									{dynamicfields.map((field, index) => (
-										<div className="b-line" key={index}>
-											<div className="input-field">
-												{/* for custodian */}
-												{(componentPage === '/custodian') &&
-												(<>
-													<label htmlFor={`fault-${index}`}>Fault:</label>
-													<select id={`fault-${index}`} name={`fault-${index}`}>
-													{faults.map(fault => (
-														<option key={fault} value={fault}>
-														{fault}
-														</option>
-													))}
-													</select>
-												</>)}
-												{/* for workshop */}
-												{(componentPage === '/workshop') &&
-												(<>
-													<label htmlFor={`${item[1]}-${index}`}>{item[2]}:</label>
-													<select id={`${item[1]}-${index}`} name={`${item[1]}-${index}`}>
-													{/* {fixedParts.map(fault => (
-														<option key={fault} value={fault}>
-														{fault}
-														</option>
-													))} */}
-													{/* first loop */}
-													{(mainIndex === 0) && (fixedParts.map(fault => (
-														<option key={fault} value={fault}>
-															{fault}
-														</option>
-													)))}
-													{/* second loop */}
-													{(mainIndex === 1) && (reqComponents.map(fault => (
-														<option key={fault} value={fault}>
-															{fault}
-														</option>
-													)))}
-													</select>
-												</>)}
-											</div>
-											<div className="with-rm-btn">
-												<div className="input-field">
-													{/* for custodian */}
-													{(componentPage === '/custodian') &&
-													(<>
-														<label htmlFor={`atm-number-${index}`}>ATM:</label>
-														<select id={`atm-number-${index}`} name={`atm-number-${index}`}>
-														{Array.from({ length: formValues.totalNumberOfATMs }, (_, i) => i + 1).map((number) => (
-															<option key={number} value={number}>
-															{number}
-															</option>
-														))}
-														</select>
-													</>)}
-													{/* for workshop */}
-													{(componentPage === '/workshop') &&
-													(<>
-														<label htmlFor={`${item[3]}-${index}`}>Quantity:</label>
-														<select id={`${item[3]}-${index}`} name={`${item[3]}-${index}`}>
-														{Array.from({ length: 16 }, (_, i) => i + 1).map((number) => (
-															<option key={number} value={number}>
-															{number}
-															</option>
-														))}
-														</select>
-													</>)}
-												</div>
-												<div>
-													<button
-														type="button"
-														onClick={() => removedynamicfieldsFieldSet(index)}
-														>Remove
-													</button>
-												</div>
-											</div>
-										</div>
-									))}
-									</div>
-								</div>
+							<div key={mainIndex}>
 								<div>
-									<div className="input-field textarea-box">
-										<label htmlFor="other-faults">Other Faults/Details:</label>
-										<textarea
-										type="text"
-										name="other-faults"
-										id="other-faults"
-										placeholder="Pls, specify ..."
-										rows={3}
-										onChange={handleInputChange}
-										/>
-									</div>
-								</div>
-							</div>
-							<div className="cust-button">
-								{/* for custodian */}
-								{(componentPage === '/custodian') && (<button onClick={EditHandler}>{!isEditable ? 'Edit Fields' : 'Done Editing'}</button>)}
-								{(componentPage === '/custodian') && (<button>Update Fields</button>)}
-								{/* <button onClick={handleUpdateFields}>Update Fields</button> */}
-								<button type="submit">
 									{/* for custodian */}
 									{(componentPage === '/custodian') &&
-									('Log Fault')}
-									{/* for workshop */}
-									{(componentPage === '/workshop') &&
-									(`${item[4]}`)}
-								</button>
-								<button type="button" onClick={addFieldSet}>
-									Add More
-								</button>
+									(<h4>Having difficulty with your machine?</h4>)}
+									{/* for workshop/engineer/human-resource */}
+									{(componentPage === '/workshop' ||
+										componentPage === '/engineer' ||
+										componentPage === '/human-resource'
+									) &&
+									(<h4>{item[0]}</h4>)}
+								</div>
+								<div>
+									{/* for custodian */}
+									{(componentPage === '/custodian') &&
+									(<div className="to-form">
+										<h4>Log a Fault</h4>
+									</div>)}
+									<hr />
+									<form onSubmit={handleSubmit}>
+										<div>
+											{/* for custodian */}
+											{(componentPage === '/custodian') &&
+											(<div className="cust-row">
+												<div className="input-field">
+													<label htmlFor="state">State:</label>
+													<input
+													type="text"
+													name="state"
+													id="state"
+													value={formValues.state}
+													onChange={handleInputChange}
+													// readOnly={!isEditable}
+													readOnly={(componentPage === pageName && !isEditable) ? true : false}
+													/>
+													{errors.state && <span className="error">{errors.state}</span>}
+												</div>
+												<div className="input-field">
+													<label htmlFor="branch">Branch:</label>
+													<input
+													type="text"
+													name="branch"
+													id="branch"
+													value={formValues.branch}
+													onChange={handleInputChange}
+													// readOnly={!isEditable}
+													readOnly={(componentPage === pageName && !isEditable) ? true : false}
+													/>
+													{errors.branch && <span className="error">{errors.branch}</span>}
+												</div>
+											</div>)}
+
+											<div className="cust-row-form dynamic-cust-row">
+												{/* <div className="dynamic-cust-row"> */}
+												{dynamicfields.map((field, index) => (
+													<div className="b-line" key={index}>
+														<>
+															<div className="input-field">
+																{/* <hr/> */}
+																{/* for custodian */}
+																{(componentPage === '/custodian') &&
+																(<>
+																	{/* <hr/> */}
+																	<label htmlFor={`fault-${index}`}>Fault:</label>
+																	<select id={`fault-${index}`} name={`fault-${index}`}>
+																	{faults.map(fault => (
+																		<option key={fault} value={fault}>
+																		{fault}
+																		</option>
+																	))}
+																	</select>
+																</>)}
+																{/* for workshop/engineer/human-resource */}
+																{(componentPage === '/workshop' ||
+																	componentPage === '/engineer' ||
+																	componentPage === '/human-resource'
+																) &&
+																(<>
+																	<label htmlFor={`${item[1]}-${index}`}>{item[2]}:</label>
+																	<select id={`${item[1]}-${index}`} name={`${item[1]}-${index}`}>
+																	{/* {fixedParts.map(fault => (
+																		<option key={fault} value={fault}>
+																		{fault}
+																		</option>
+																	))} */}
+																	{/* first loop */}
+																	{(mainIndex === 0) && (fixedParts.map(fault => (
+																		<option key={fault} value={fault}>
+																			{fault}
+																		</option>
+																	)))}
+																	{/* second loop */}
+																	{(mainIndex === 1) && (reqComponents.map(fault => (
+																		<option key={fault} value={fault}>
+																			{fault}
+																		</option>
+																	)))}
+																	</select>
+																</>)}
+															</div>
+															<div className="with-rm-btn">
+																<div className="input-field">
+																	{/* for custodian */}
+																	{(componentPage === '/custodian') &&
+																	(<>
+																		<label htmlFor={`atm-number-${index}`}>ATM:</label>
+																		<select id={`atm-number-${index}`} name={`atm-number-${index}`}>
+																		{Array.from({ length: formValues.totalNumberOfATMs }, (_, i) => i + 1).map((number) => (
+																			<option key={number} value={number}>
+																			{number}
+																			</option>
+																		))}
+																		</select>
+																	</>)}
+																	{/* for workshop/engineer/human-resource */}
+																	{(componentPage === '/workshop' ||
+																		componentPage === '/engineer' ||
+																		componentPage === '/human-resource'
+																	) &&
+																	(<>
+																		<label htmlFor={`${item[3]}-${index}`}>Quantity:</label>
+																		<select id={`${item[3]}-${index}`} name={`${item[3]}-${index}`}>
+																		{/* for workshop/engineer */}
+																		{(componentPage !== '/human-resource') &&
+																		(Array.from({ length: 16 }, (_, i) => i + 1).map((number) => (
+																			<option key={number} value={number}>
+																			{number}
+																			</option>
+																		)))}
+																		{/* for human-resource */}
+																		{(componentPage === '/human-resource') &&
+																		(Array.from({ length: 100 }, (_, i) => i + 1).map((number) => (
+																			<option key={number} value={number}>
+																			{number}
+																			</option>
+																		)))}
+																		</select>
+																	</>)}
+																</div>
+																<div>
+																	<button
+																		type="button"
+																		onClick={() => removedynamicfieldsFieldSet(index)}
+																		>Remove
+																	</button>
+																</div>
+															</div>
+														</>
+													</div>
+												))}
+												</div>
+											</div>
+											<div>
+												<div className="input-field textarea-box">
+													<label htmlFor={`other-${mainIndex}`}>Others:</label>
+													<textarea
+													type="text"
+													name={`other-${mainIndex}`}
+													id={`other-${mainIndex}`}
+													placeholder="Pls, specify ..."
+													rows={3}
+													onChange={handleInputChange}
+													/>
+												{/* </div> */}
+											</div>
+										</div>
+										<div className="cust-button">
+											{/* for custodian */}
+											{(componentPage === '/custodian') && (<button onClick={EditHandler}>{!isEditable ? 'Edit Fields' : 'Done Editing'}</button>)}
+											{(componentPage === '/custodian') && (<button>Update Fields</button>)}
+											{/* <button onClick={handleUpdateFields}>Update Fields</button> */}
+											<button type="submit">
+												{/* for custodian */}
+												{(componentPage === '/custodian') &&
+												('Log Fault')}
+												{/* for workshop/engineer/human-resource */}
+												{(componentPage === '/workshop' ||
+													componentPage === '/engineer' ||
+													componentPage === '/human-resource'
+												) &&
+												(`${item[4]}`)}
+											</button>
+											<button type="button" onClick={addFieldSet}>
+												Add More
+											</button>
+										</div>
+									</form>
+									<hr/>
+								</div>
 							</div>
-						</form>
-					</div>
 					</>)})}
 			</div>
 		</>
