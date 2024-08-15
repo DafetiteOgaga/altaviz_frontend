c461475b972a337215c631a7887209ccb223d531
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-15 06:53:08 +0100

    fix Engineer.js: fixed toggle for post and request forms

--- altaviz/src/components/SideBar/engineer/Engineer.js
+++ altaviz/src/components/SideBar/engineer/Engineer.js
@@ -1,7 +1,10 @@
 import LogFault from "../log_fault/LogFault";
 import Dashboard from "../dashboard/Dashboard";
+import { useState } from "react";
 
 function Engineer() {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggleOpen = () => setIsOpen(!isOpen);
 	const name = "engineer";
 	const childList = [
 		['Request for parts', 'part', 'Request for Part', 'part-qty', 'Request Part'],
@@ -12,7 +15,10 @@ function Engineer() {
 			<div className="background-color custodian-page">
 				<Dashboard name={name} pageName={`/${name}`} />
 				<hr style={{width: '80%'}} />
-				<LogFault childList={childList} />
+				<div className="custum-button">
+					<h5 onClick={toggleOpen}>{isOpen ? 'Collapse Form' : 'Request parts/Components'}</h5>
+				</div>
+				{isOpen && (<LogFault childList={childList} />)}
 			</div>
 		</>
 	);
