8bff64eaec77b8cfe6b652a9b3135ba45621eb50
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-15 06:50:37 +0100

    fix Dashboard.js: fixed dashboard to handle data rendering for different pages

--- altaviz/src/components/SideBar/dashboard/Dashboard.js
+++ altaviz/src/components/SideBar/dashboard/Dashboard.js
@@ -1,10 +1,15 @@
 import cMockData from "../custodian/custodianMockData";
 import wMockData from "../workshop/workshopMockData";
+import eMockData from "../engineer/engineerMockData";
+import hdMockData from "../help_desk/helpdeskMockData";
+import sMockData from "../supervisor/supevisorMockData";
+import hrMockData from "../human_resource/humanresourceMockData";
 import "../sidebar_pages.css";
 import CustomTime from "../../hooks/CustomTime";
 import { Link, useLocation } from "react-router-dom";
-import Resolved from "../requestApprovedPendingResolved/resolved/Resolved";
-import PendingFaults from "../requestApprovedPendingResolved/pending_faults/PendingFaults";
+import Resolved from "../notifications/resolved/Resolved";
+import PendingFaults from "../notifications/pending_faults/PendingFaults";
+import PendingRequest from "../notifications/pendingRequest/pendingRequest";
 
 function Dashboard({name, pageName}) {
 	// const name = 'Paul';
@@ -16,9 +21,12 @@ function Dashboard({name, pageName}) {
 	// }
 	const { location, banks, locations } = cMockData();
 	const { location:wuser } = wMockData();
+	const { location:euser } = eMockData();
+	const { location:hduser } = hdMockData();
+	const { location:suser } = sMockData();
+	const { user:hruser } = hrMockData();
 	return (
 		<>
-			
 			<CustomTime name={name} />
 			<div className="dash-form">
 				<h4>Dashboard</h4>
@@ -36,6 +44,26 @@ function Dashboard({name, pageName}) {
 							(<p><strong>Department: </strong>
 								{wuser.user.department.dept}
 							</p>)}
+							{/* engineer */}
+							{(componentPage === '/engineer') &&
+							(<p><strong>Department: </strong>
+								{euser.user.department.dept}
+							</p>)}
+							{/* help-desk */}
+							{(componentPage === '/help-desk') &&
+							(<p><strong>Department: </strong>
+								{hduser.user.department.dept}
+							</p>)}
+							{/* supervisor */}
+							{(componentPage === '/supervisor') &&
+							(<p><strong>Department: </strong>
+								{suser.user.department.dept}
+							</p>)}
+							{/* human-resource */}
+							{(componentPage === '/human-resource') &&
+							(<p><strong>Department: </strong>
+								{hruser.department.department}
+							</p>)}
 						</div>
 
 						<div className="input-field">
@@ -49,6 +77,26 @@ function Dashboard({name, pageName}) {
 							(<p><strong>Email: </strong>
 								{wuser.user.email}
 							</p>)}
+							{/* engineer */}
+							{(componentPage === '/engineer') &&
+							(<p><strong>Email: </strong>
+								{euser.user.email}
+							</p>)}
+							{/* help-desk */}
+							{(componentPage === '/help-desk') &&
+							(<p><strong>Email: </strong>
+								{hduser.user.email}
+							</p>)}
+							{/* supervisor */}
+							{(componentPage === '/supervisor') &&
+							(<p><strong>Email: </strong>
+								{suser.user.email}
+							</p>)}
+							{/* human-resource */}
+							{(componentPage === '/human-resource') &&
+							(<p><strong>Email: </strong>
+								{hruser.email}
+							</p>)}
 						</div>
 						<div className="input-field">
 							{/* custodian */}
@@ -58,8 +106,28 @@ function Dashboard({name, pageName}) {
 							</p>)}
 							{/* workshop */}
 							{(componentPage === '/workshop') &&
+							(<p><strong>Deliveries: </strong>
+								{wuser.user.department.numberOfPartsDelivered}
+							</p>)}
+							{/* engineer */}
+							{(componentPage === '/engineer') &&
+							(<p><strong>Deliveries: </strong>
+								{euser.user.department.numberOfPartsDelivered}
+							</p>)}
+							{/* help-desk */}
+							{(componentPage === '/help-desk') &&
+							(<p><strong>Deliveries: </strong>
+								{hduser.user.department.numberOfPartsDelivered}
+							</p>)}
+							{/* supervisor */}
+							{(componentPage === '/supervisor') &&
 							(<p><strong>Photo: </strong>
-								{wuser.user.profile_photo}
+								{/* {suser.user.profile_photo} */}
+							</p>)}
+							{/* human-resource */}
+							{(componentPage === '/human-resource') &&
+							(<p><strong>Photo: </strong>
+								{/* {hruser.profile_photo} */}
 							</p>)}
 						</div>
 					</div>
@@ -83,6 +151,42 @@ function Dashboard({name, pageName}) {
 									{wuser.user.name}
 								</Link>
 							</p>)}
+							{/* engineer */}
+							{(componentPage === '/engineer') &&
+							(<p><strong>Engineer: </strong>
+								<Link
+								style={{color: '#333'}}
+								to="/user/1">
+									{euser.user.name}
+								</Link>
+							</p>)}
+							{/* help-desk */}
+							{(componentPage === '/help-desk') &&
+							(<p><strong>User: </strong>
+								<Link
+								style={{color: '#333'}}
+								to="/user/1">
+									{hduser.user.name}
+								</Link>
+							</p>)}
+							{/* supervisor */}
+							{(componentPage === '/supervisor') &&
+							(<p><strong>User: </strong>
+								<Link
+								style={{color: '#333'}}
+								to="/user/1">
+									{suser.user.name}
+								</Link>
+							</p>)}
+							{/* human-resource */}
+							{(componentPage === '/human-resource') &&
+							(<p><strong>User: </strong>
+								<Link
+								style={{color: '#333'}}
+								to="/user/1">
+									{hruser.names.firstname}
+								</Link>
+							</p>)}
 						</div>
 						<div className="input-field">
 							{/* custodian */}
@@ -99,6 +203,26 @@ function Dashboard({name, pageName}) {
 							(<p><strong>Phone No.: </strong>
 								{wuser.user.phone}
 							</p>)}
+							{/* engineer */}
+							{(componentPage === '/engineer') &&
+							(<p><strong>Phone No.: </strong>
+								{euser.user.phone}
+							</p>)}
+							{/* help-desk */}
+							{(componentPage === '/help-desk') &&
+							(<p><strong>Phone No.: </strong>
+								{hduser.user.phone}
+							</p>)}
+							{/* supervisor */}
+							{(componentPage === '/supervisor') &&
+							(<p><strong>Phone No.: </strong>
+								{suser.user.phone}
+							</p>)}
+							{/* human-resource */}
+							{(componentPage === '/human-resource') &&
+							(<p><strong>Phone No.: </strong>
+								{hruser.phone}
+							</p>)}
 						</div>
 						<div className="input-field">
 							{/* custodian */}
@@ -111,12 +235,44 @@ function Dashboard({name, pageName}) {
 							(<p><strong>Whatsapp No.: </strong>
 								{wuser.user.whatsapp}
 							</p>)}
+							{/* engineer */}
+							{(componentPage === '/engineer') &&
+							(<p><strong>Whatsapp No.: </strong>
+								{wuser.user.whatsapp}
+							</p>)}
+							{/* help-desk */}
+							{(componentPage === '/help-desk') &&
+							(<p><strong>Whatsapp No.: </strong>
+								{hduser.user.whatsapp}
+							</p>)}
+							{/* supervisor */}
+							{(componentPage === '/supervisor') &&
+							(<p><strong>Whatsapp No.: </strong>
+								{suser.user.whatsapp}
+							</p>)}
+							{/* human-resource */}
+							{(componentPage === '/human-resource') &&
+							(<p><strong>Whatsapp No.: </strong>
+								{hruser.wphone}
+							</p>)}
 						</div>
 					</div>
-					<div className="pend-resol">
-						<PendingFaults />
-						<Resolved text={message} />
-					</div>
+					{/* engineer */}
+					{(componentPage !== '/workshop') &&
+					(<div className="pend-resol">
+						{componentPage !== '/human-resource' && <PendingFaults />}
+						{/* help-desk/supervisor/human-resource */}
+						{(componentPage === '/help-desk' ||
+							componentPage === '/supervisor' ||
+							componentPage === '/human-resource'
+						) && (<PendingRequest />)}
+						{(componentPage !== '/help-desk' &&
+							componentPage !== '/supervisor' &&
+							componentPage !== '/human-resource'
+						) && (
+							<Resolved componentPage={componentPage.split('/')[1]} text={message} />
+						)}
+					</div>)}
 				</div>
 			</div>
 		</>
