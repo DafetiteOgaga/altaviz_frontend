d1304fdf1b6b5a0ee4ea105a0ea4e00c8673d23a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-15 06:36:18 +0100

    fix Navigation.js: fixed dropdown menu import path

--- altaviz/src/components/header/Navigation.js
+++ altaviz/src/components/header/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import DropdownMenu from './DropdownMenu';
+import DropdownMenu from './product_dropdown_menu/DropdownMenu';
 
 function Navigation() {
   return (
