da5912ed32e9929ae995134773a4b2e90209fd6d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-20 12:01:39 +0100

    test

--- altaviz/src/components/Testfetchapi.js
+++ altaviz/src/components/Testfetchapi.js
@@ -1,38 +1,11 @@
-// import useFetch from "./hooks/useFetch";
-// import { useContext } from "react";
-// import { GlobalContext } from "./context/Context";
-// import GetProductDetails from "./hooks/GetProductDetails";
-// import Dashboard from "./SideBar/dashboard/Dashboard";
-// import LogFault from "./SideBar/log_fault/LogFault";
+import AddComponent from "./SideBar/componentComps/AddComponent";
 
 
 function Testfetchapi() {
-	// const { useFetchGET } = useContext(GlobalContext)
-	// const { data, loading, error } = useFetchGET('http://localhost:8000');
-
-	// if (loading) {
-	// 	console.log('Loading ...');
-	// 	return (
-	// 		<>
-	// 			<h6 className="error-and-loading">Loading...</h6>
-	// 		</>
-	// );};
-	// if (error) {
-	// 	console.log('Error ...');
-	// 	return (
-	// 		<>
-	// 			<h6 className="error-and-loading">yo! {error}</h6>
-	// 		</>
-	// );};
-	// console.log(data);
-
 	return (
 		<>
 			<h2>Test Page</h2>
-			<div className="background-color custodian-page">
-				{/* <Dashboard />
-				<LogFault /> */}
-			</div>
+			<AddComponent />
 		</>
 	);
 }
