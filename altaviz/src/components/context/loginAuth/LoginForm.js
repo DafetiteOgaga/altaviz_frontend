608e25714c7edac3d1dfa8446d9233b0c362f652
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-26 00:16:55 +0100

    new LoginForm.js: uncompleted but functional login form

---
+++ altaviz/src/components/context/loginAuth/LoginForm.js
@@ -0,0 +1,59 @@
+import { useState, useContext } from "react";
+import { LoginContext } from "./LoginOutContext";
+
+function LoginForm() {
+	const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+	// const [loginTrigger, setLoginTrigger] = useState(false);
+
+	// post setup
+	const { Login, authData, authLoading, authError } = useContext(LoginContext);
+	const handleLogin = async () => {
+		const result = await Login(email, password, true);
+		// const result = await Login(email, password, loginTrigger);
+			// triggerLogin();
+			// console.log('loginTrigger (submithandle):', loginTrigger);
+		if (result !== null && result.success) {
+			console.log('Login successful', result.data);
+		} else {
+			console.log('Login failed', result);
+		}
+			// setLoginTrigger(false);ogagadafetite@gmail.com
+	};
+	// const triggerLogin = () => {
+	// 	console.log('triggering login #####');
+	// 	setLoginTrigger(true);
+	// 	console.log('loginTrigger (triggerhandle):', loginTrigger);
+	// };
+	console.log('email:', email);
+	console.log('password:', password);
+	// console.log('loginTrigger:', loginTrigger);
+	console.log('authData:', authData);
+	return (
+		<div>
+			<form onSubmit={(e) => {
+				e.preventDefault();
+				handleLogin()}}>
+				<input
+					type="email"
+					placeholder="Email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button
+				type="submit"
+				disabled={authLoading}>
+					{authLoading ? "Logging in..." : "Login"}
+				</button>
+				{authError && <p style={{ color: 'red' }}>{authError}</p>}
+			</form>
+		</div>
+	)
+}
+export default LoginForm;
