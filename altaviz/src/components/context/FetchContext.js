a3c623bb614d1b36d8459dbecb36c08e28e19743
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-08-26 00:27:45 +0100

    fix FetchContext.js: fix FetchContext.js: fixed the useGetDataAPI api function

--- altaviz/src/components/context/FetchContext.js
+++ altaviz/src/components/context/FetchContext.js
@@ -3,16 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 export const FetchContext = createContext();
 export const FetchProviders = ({ children }) => {
-	const [getData, setGetData] = useState(null);
-	const [getLoading, setGetLoading] = useState(true);
-	const [getError, setGetError] = useState(null);
-
 	const [postData, setPostData] = useState(null);
 	const [postLoading, setPostLoading] = useState(false);
 	const [postError, setPostError] = useState(null);
 	const redirectTo = useNavigate()
 
 	function useGetDataAPI(url) {
+		const [getData, setGetData] = useState(null);
+		const [getLoading, setGetLoading] = useState(true);
+		const [getError, setGetError] = useState(null);
 		useEffect(() => {
 			const fetchData = async () => {
 				try {
@@ -33,75 +32,20 @@ export const FetchProviders = ({ children }) => {
 		return { getData, getLoading, getError };
 	}
 
-	// function usePostDataAPI(url, formData, trigger, redirectToPage) {
-	// 	useEffect(() => {
-	// 		const PostData = async () => {
-	// 			setPostLoading(true);
-	// 			try {
-	// 				const response = await fetch(url, {
-	// 					method: 'POST',
-	// 					// headers: {
-	// 					// 	'Content-Type': 'application/json',
-	// 					// },
-	// 					// body: JSON.stringify(formData),
-	// 					body: formData
-	// 				});
-	// 				if (response.ok) {
-	// 					const responseData = await response.json();
-	// 					// alert('Form submitted successfully');
-	// 					// console.dir('payload (stringify): ' + JSON.stringify(formData));
-	// 					// console.dir('payload after (not stringify): ' + formData);
-	// 					setPostData(responseData)
-	// 					// setLoading(false);
-	// 					// console.log('Response from server:', responseData);
-
-	// 					// remove this alert
-	// 					alert(Object.entries(responseData)
-	// 							.map(([key, value]) => `${key}: ${value}\n`)); // remove
-	// 					// console.log('response:', response);
-	// 					// console.log('response.ok:', response.ok);
-	// 					// console.log('responseData:', responseData);
-	// 					// console.log('data:', data);
-	// 					// if (data) {
-	// 					// 	alert(data.status)
-	// 					// }
-	// 					if (redirectToPage) {
-	// 						redirectTo('/success');
-	// 						setTimeout(() => {
-	// 							redirectTo(redirectToPage);
-	// 						}, 2500);
-	// 					}
-	// 					// redirectTo('/');
-
-	// 					// onSuccess(responseData);
-	// 				} else {
-	// 					// setLoading(false);
-	// 					alert('Failed to submit the form');
-	// 					throw new Error('Could not post request.');
-	// 					// setPrevData(fields);
-	// 					// setPrevData(formData);
-	// 				}
-	// 			} catch (postError) {
-	// 				// setLoading(false);
-	// 				setPostError(postError.message);
-	// 				console.log('server error:', postError.message);
-	// 				console.error('Error submitting the form:', postError);
-	// 			} finally {
-	// 				setPostLoading(false);
-	// 			}
-	// 		};
-	// 		if (trigger) {
-	// 			PostData();
-	// 		}
-	// 		// PostData();
-	// 	}, [url, formData, trigger, redirectToPage]);
-	// 	return { postData, postLoading, postError };
-	// }
 	function usePostDataAPI(url, formData, trigger, redirectToPage) {
 		// const [trigger, setTrigger] = useState(false);
-	
 		useEffect(() => {
-			if (!trigger) return;
+			if (!trigger) {
+				console.log('Login trigger is false. POST call not executed.');
+				return;
+			}
+			// delete
+			// Inspection
+			console.log('from post api #####')
+			console.log('url:', url);
+			for (const [key, value] of formData.entries()) {
+				console.log(`item: ${key}\nvalue: ${value}`);
+			}
 			const postData = async () => {
 				try {
 				setPostLoading(true);
@@ -114,10 +58,10 @@ export const FetchProviders = ({ children }) => {
 					setPostData(responseData);
 
 					if (redirectToPage) {
-					redirectTo("/success");
-					setTimeout(() => {
-						redirectTo(redirectToPage);
-					}, 2500);
+						redirectTo("/success");
+						setTimeout(() => {
+							redirectTo(redirectToPage);
+						}, 2500);
 					}
 				} else {
 					throw new Error("Could not post request.");
@@ -148,5 +92,3 @@ export const FetchProviders = ({ children }) => {
 		</FetchContext.Provider>
 	);
 }
-
-
