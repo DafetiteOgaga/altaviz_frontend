08a5a8ad94670f1fd86a267d72cd426458fbf5a2
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-10-26 23:41:50 +0100

    fixed up Engineer.js: engineers can now make requests but only to a fault and perform other neccessary role based activities

--- altaviz/src/components/SideBar/engineer/Engineer.js
+++ altaviz/src/components/SideBar/engineer/Engineer.js
@@ -1,27 +1,196 @@
-import LogFault from "../log_fault/LogFault";
+// import LogFault from "../custodian/log_fault/LogFault";
+import UpdateUser from "../human_resource/createAndUpdateUserForms/updateUser";
 import Dashboard from "../dashboard/Dashboard";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../context/checkAuth/AuthContext";
+import Notification from "../notification/Notification";
+import FaultListDisplay from "../faults/userAndFaultDisplay/FaultListDisplay";
 
 function Engineer() {
-	const [isOpen, setIsOpen] = useState(false);
-	const toggleOpen = () => setIsOpen(!isOpen);
-	const name = "engineer";
-	const childList = [
-		['Request for parts', 'part', 'Request for Part', 'part-qty', 'Request Part'],
-		['Request for components', 'comp', 'Request Components', 'comp-qty', 'Request Component'],
-	]
+	const [isUserDetailsFormOpen, setIsUserDetailsFormOpen] = useState(false);
+	const toggleUserDetailsForm = () => setIsUserDetailsFormOpen(!isUserDetailsFormOpen);
+	const activeStyles = {
+		backgroundColor: '#8A8A93',
+	}
+	const notificationStyles = {
+		display: 'flex',
+		justifyContent: 'space-around',
+		padding: '1rem 0'
+	}
 	return (
 		<>
 			<div className="background-color custodian-page">
-				<Dashboard name={name} pageName={`/${name}`} />
+				<Dashboard />
 				<hr style={{width: '80%'}} />
-				<div className="custum-button">
-					<h5 onClick={toggleOpen}>{isOpen ? 'Collapse Form' : 'Request parts/Components'}</h5>
+				<div style={notificationStyles}>
+					{/* pending component requests */}
+					<Notification
+						urlPath='request-component'
+						variableContext='componentKey'
+						totalArrayContext='totalcomponentKey'
+						titleKey='Pending Component Requests'
+						titleValue='No Pending Requests'
+						// patchUrl={patchUrl}
+						// postUrl={postUrl}
+						// putUrl={putUrl}
+						// putData={putData}
+						deleteUrl='request-component'
+						handler='requestHandler'
+						// listHandle
+						// requestHandler
+						// unapprovedHandler
+						listPageUrl='component-request-list'
+						detailPageUrl='component-request-details'
+						// refreshKeyList={refreshKeyList}
+						button='Withdraw'
+						extraDisplayLocalKeys={[
+							'componentKey', 'totalcomponentKey',
+							'partKey', 'totalpartKey',
+							'partPendingList',	'componentPendingList',
+							'workshopRequests'
+						]}
+						/>
+
+					{/* pending part requests */}
+					<Notification
+						urlPath='request-part'
+						variableContext='partKey'
+						totalArrayContext='totalpartKey'
+						titleKey='Pending Parts Requests'
+						titleValue='No Pending Requests'
+						// patchUrl={patchUrl}
+						// postUrl={postUrl}
+						// putUrl={putUrl}
+						// putData={putData}
+						deleteUrl='request-part'
+						handler='requestHandler'
+						// listHandle
+						// requestHandler
+						// unapprovedHandler
+						listPageUrl='part-request-list'
+						detailPageUrl='part-request-details'
+						// refreshKeyList={refreshKeyList}
+						button='Withdraw'
+						extraDisplayLocalKeys={[
+							'componentKey', 'totalcomponentKey',
+							'partKey', 'totalpartKey',
+							'partPendingList',	'componentPendingList',
+							'workshopRequests'
+						]}
+						/>
+
+					{/* <Notification
+						urlPath='engineer-pending-faults'
+
+						faultsKey
+						totalfaultsKey
+						unconfirmedKey
+						totalunconfirmedKey
+
+						variableContext='faultsKey'
+						totalArrayContext='totalfaultsKey'
+						titleKey='Pending Faults'
+						titleValue='No Pending Faults'
+						patchUrl={patchUrl}
+						postUrl={postUrl}
+						putUrl={putUrl}
+						putData={putData}
+						deleteUrl={deleteUrl}
+						listPageUrl={listPageUrl}
+						detailPageUrl={detailPageUrl}
+						refreshKeyList={refreshKeyList}
+						button={button}
+						extraDisplayLocalKeys={['faultpendingList']}
+						/> */}
+
+					{/* <PendingRequestNotifi authData={authData}/> */}
+					{/* <PendingPartsNotifi authData={authData}/> */}
+					{/* <ConfirmResolutionRequestNotifi authData={authData}/> */}
 				</div>
-				{isOpen && (<LogFault childList={childList} />)}
+				<hr style={{width: '45%'}} />
+				<div style={notificationStyles}>
+					{/* pending faults */}
+					<Notification
+						urlPath='engineer-pending-faults'
+						variableContext='faultsKey'
+						totalArrayContext='totalfaultsKey'
+						titleKey='Pending Faults'
+						titleValue='No Pending Faults'
+						patchUrl='pending-faults'
+						handler='listHandle'
+						// listHandle
+						// requestHandler
+						// unapprovedHandler
+						listPageUrl='pending/fault-gen-list'
+						detailPageUrl='pending/fault-gen-details'
+						button='Seek Confirmation'
+						extraDisplayLocalKeys={[
+							'faultsKey', 'totalfaultsKey',
+							'unconfirmedKey', 'totalunconfirmedKey',
+							'faultpendingList',	'faultunconfirmedList',
+							'unresolvedKey'
+						]}
+						/>
+
+						{/* faultsKey
+						totalfaultsKey
+						unconfirmedKey
+						totalunconfirmedKey */}
+
+					{/* pending unconfirmed resolutions */}
+					<Notification
+						urlPath='engineer-unconfirmed-faults'
+						variableContext='unconfirmedKey'
+						totalArrayContext='totalunconfirmedKey'
+						titleKey='Unconfirmed Resolutions'
+						titleValue='All Resolutions Confirmed'
+						handler='listHandle'
+						// listHandle
+						// requestHandler
+						// unapprovedHandler
+						listPageUrl='unconfirmed/fault-gen-list'
+						detailPageUrl='unconfirmed/fault-gen-details'
+						extraDisplayLocalKeys={[
+							'faultsKey', 'totalfaultsKey',
+							'unconfirmedKey', 'totalunconfirmedKey',
+							'faultpendingList',	'faultunconfirmedList',
+							'unresolvedKey'
+						]}
+						/>
+				</div>
+				<hr style={{width: '80%'}} />
+				<div
+				style={{
+					display: 'flex',
+					justifyContent: 'space-evenly'
+				}}>
+					{/* <div className="custum-button">
+						<h5
+						style={isCompRequestFormOpen ? {...activeStyles} : {}}
+						onClick={toggleCompRequestForm}>{isCompRequestFormOpen ? 'Close Form' : 'Request Components'}</h5>
+					</div> */}
+					{/* <div className="custum-button">
+						<h5
+						style={isPartRequestFormOpen ? {...activeStyles} : {}}
+						onClick={togglePartRequestForm}>{isPartRequestFormOpen ? 'Close Form' : 'Request Parts'}</h5>
+					</div> */}
+					<div className="custum-button">
+						<h5
+						style={isUserDetailsFormOpen ? {...activeStyles} : {}}
+						onClick={toggleUserDetailsForm}>{isUserDetailsFormOpen ? 'Close Form' : 'Update Details'}</h5>
+					</div>
+				</div>
+				{/* {isCompRequestFormOpen && (<RequestComponentForm />)}
+				{isPartRequestFormOpen && (<RequestPartForm />)} */}
+				{/* {isPartRequestFormOpen && (<AddPartForm />)} */}
+				{isUserDetailsFormOpen && (<UpdateUser />)}
+				<FaultListDisplay
+					faultUrl='engineer-unresolved-faults'
+					faultKeyContext='unresolvedKey'/>
 			</div>
 		</>
 	);
 };
+
 
 export default Engineer;
