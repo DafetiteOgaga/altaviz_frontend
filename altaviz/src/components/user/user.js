0a710c28c814c5c8300d3a0aaffa5087ccc98cea
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-09 22:08:04 +0100

    fixed user.js: fixed the component to conform with the data from server

--- altaviz/src/components/user/user.js
+++ altaviz/src/components/user/user.js
@@ -1,57 +1,77 @@
 // import logo from "../../logo/altaviz_logo.png"
 import "./user.css"
+import { SentenceCaseContext } from "../context/SentenceCaseContext";
 import { useContext, useState, useEffect } from "react"
 import { FetchContext } from "../context/FetchContext"
+import { useParams, useLocation } from "react-router-dom";
+import useGetWEncryptionSingleItem from "../paginationComp/useGetWEncryptionSingleItem";
 
 function User () {
-	// post setup
+	const { toSentenceCase } = useContext(SentenceCaseContext);
+	// const [response, setResponse] = useState(false);
+	const [userDataToState, setuserDataToState] = useState(false);
+	const locatn = useLocation()
+	const para = useParams().id
+	console.log('location:', locatn)
+	console.log('para:', para)
 	// const [postTrigger, setPostTrigger] = useState(false);
 	// const [formData, setFormData] = useState(new FormData());
 	const { useGetDataAPI } = useContext(FetchContext);
-	// const { getData, getLoading, getError } = useGetDataAPI(
-	const { getData, getLoading } = useGetDataAPI(
-		'http://127.0.0.1:8000/user/1',
+	const { getData:userData, getError:userError, getLoading:userLoading } = useGetDataAPI(
+		`http://127.0.0.1:8000/user/${para}/`, true
 	);
-	const [response, setResponse] = useState(false);
+	// const user = useGetWEncryptionSingleItem(
+	// 	`http://127.0.0.1:8000/user/${para}/`,
+	// 	`user-${para}`,
+	// 	// isRefresh
+	// )
+	// const { getData, getLoading } = useGetDataAPI(
+	// 	// `http://127.0.0.1:8000/user/${para}/`,
+	// 	`http://127.0.0.1:8000/user/${para}/`,
+	// );
 	useEffect(() => {
-		if (getData) {
-			setResponse(true);
-			console.log('profile data:', getData)
-			// console.log('is_active:', getData.is_active)
+		if (userData) {
+			console.log('userData:', userData)
+			setuserDataToState(userData)
 		}
-	}, [getData])
-
-	// const user = {
-	// 	name: 'James',
-	// 	department: {
-	// 		dept: 'workshop',
-	// 		numberOfPartsDelivered: 52,
-	// 		numberOfPendingParts: 9,
-	// 	},
-	// 	phone: '12345678902',
-	// 	whatsapp: '98765432103',
-	// 	profile_photo: 'placeholder.png',
-	// 	email: 'james@example.com',
-	// 	address: '12, kudirat way, lagos',
-	// 	status: 'active',
-	// }
-	
+	}, [userData])
+	// useEffect(() => {
+	// 	if (user.getData) {
+	// 		// setResponse(true);
+	// 		console.log('profile data:', user.getData)
+	// 		setUserDataToStateToState(user.getData)
+	// 		// console.log('is_active:', getData.is_active)
+	// 	}
+	// }, [user.getData])
+	console.log('userData (global):', userData)
+	// console.log('getData:', user.getData)
+	console.log(`user-${para}`)
+	console.log('userDataToState:', userDataToState)
 	return (
-		response ?
-			(<>
+			<>
+			{userLoading && <h4 style={{
+				// padding: '1rem',
+				color: '#888',
+				// fontSize: '1.2rem',
+				textAlign: 'center',
+			}}>Loading...</h4>}
+			{userError && <h4>{userError}</h4>}
+			{userDataToState &&
 			<div className="background-color user-page">
 				<div className="user-info">
 					<div>
 						<div className="user-page">
-							<img src={getData.profile_picture} alt="profile pic" />
+							<img src={`http://127.0.0.1:8000/${userDataToState.profile_picture}`} alt="profile pic" />
 						</div>
-						{getData.custodian &&
+						{userDataToState.role === 'custodian' &&
 							<div className="uDetaisRight">
-								<h4>Bank: {getData.custodian.bank}</h4>
-								<h4>State: {getData.custodian.state}</h4>
-								<h4>Branch: {getData.custodian.branch}</h4>
-								<h4>Location: {(getData.custodian.location) ? (getData.custodian.location) : 'None'}</h4>
-							</div>}
+								<h4>Bank: {toSentenceCase(userDataToState.branch.bank.name)}</h4>
+								<h4>State: {toSentenceCase(userDataToState.branch.state.name)}</h4>
+								<h4>Branch: {toSentenceCase(userDataToState.branch.name)}</h4>
+								<h4>Branch Location: {toSentenceCase(userDataToState.branch.location.location)}</h4>
+								{/* <h4>Location: {(userDataToState.custodian.location) ? (userDataToState.custodian.location) : 'None'}</h4> */}
+							</div>
+							}
 					</div>
 					<div
 					style={{
@@ -62,29 +82,28 @@ function User () {
 						}}
 					></div>
                     <div className="user-page uDetaisLeft">
-						<h4>First Name: {getData.first_name}</h4>
-						<h4>Middle Name: {getData.middle_name}</h4>
-						<h4>Last Name: {getData.last_name}</h4>
-						<h4>Username: {getData.username}</h4>
-						<h4>Phone: {getData.phone}</h4>
-						<h4>Whatsapp: {getData.wphone}</h4>
-						<h4>Email: {getData.email}</h4>
-						<h4>Department: {getData.department}</h4>
-						<h4>deliveries: {getData.deliveries}</h4>
-						<h4>Pendings: {getData.pendings}</h4>
-						<h4>Address: {getData.address}</h4>
-						<h4>Status: {!getData.is_deleted ? 'Yes' : 'No'}</h4>
-						<h4>About Me: {getData.aboutme}</h4>
+						<h4>First Name: {toSentenceCase(userDataToState.first_name)}</h4>
+						<h4>Middle Name: {toSentenceCase(userDataToState.middle_name)}</h4>
+						<h4>Last Name: {toSentenceCase(userDataToState.last_name)}</h4>
+						<h4>Username: {toSentenceCase(userDataToState.username)}</h4>
+						<h4>Phone: {userDataToState.phone}</h4>
+						<h4>Whatsapp: {userDataToState.wphone}</h4>
+						<h4>Email: {userDataToState.email}</h4>
+						<h4>Role: {toSentenceCase(userDataToState.role)}</h4>
+						<h4>Location: {toSentenceCase(userDataToState.location.location)}</h4>
+						<h4>Region: {toSentenceCase(userDataToState.region.name)}</h4>
+						{userDataToState.role !== 'custodian' &&
+							<>
+								<h4>deliveries: {userDataToState.deliveries}</h4>
+								<h4>Pendings: {userDataToState.pendings}</h4>
+							</>}
+						<h4>Address: {toSentenceCase(userDataToState.address)}</h4>
+						<h4>Status: {!userDataToState.is_deleted ? 'Active' : 'Inactive'}</h4>
+						<h4>About Me: {userDataToState.aboutme && toSentenceCase(userDataToState.aboutme)}</h4>
 					</div>
                 </div>
-            </div>
-		</>) : (<h4>{getLoading}</h4>)
+            </div>}
+		</>
 	)
 }
 export default User;
-
-// deliveries
-// 'id', 'first_name', 'last_name', 'middle_name',
-// 'username', 'email', 'phone', 'wphone', 'address',
-// 'department', 'deliveries', 'profile_picture', 'aboutme',
-// 'password'
