66f737631e7c995a4a22b96c2cf9b6a354edc452
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-09 21:14:50 +0100

    fixed Inventory.js: renders the state and information of the current inventory

--- altaviz/src/components/SideBar/human_resource/inventory/Inventory.js
+++ altaviz/src/components/SideBar/human_resource/inventory/Inventory.js
@@ -1,28 +1,105 @@
-import CustomTime from "../../../hooks/CustomTime";
-import InventoryMockData from "./inventoryData";
 import "./inventory.css";
-import { useContext, useState } from "react";
-import { FetchContext } from "../../../context/FetchContext";
-// import { useState } from "react";
-import AddComponentName from "./AddComponentName";
-import AddPartName from "./AddPartName";
+import { useContext, useState, useEffect } from "react";
+// import AddComponentName from "./AddComponentName";
+// import AddPartName from "./AddPartName";
+import { RotContext } from "../../../context/RotContext";
+import useGetWithEncryption from "../../../paginationComp/useGetWithEncryption";
+// import { FaSync, FaSpinner } from 'react-icons/fa';
+import { useRefreshContext } from "../../../context/RefreshContext";
+// import { useNavigate}
+import { SentenceCaseContext } from "../../../context/SentenceCaseContext";
+import AddInventoryName from "./AddInventoryName";
+// import { useNavigate, useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 
 function Inventory() {
-	// const { fetchData, fetchPost, fetchPut, fetchDelete } = useContext(FetchContext);
-	const { useGetDataAPI } = useContext(FetchContext);
-	const {
-		data: componentsData,
-		loading: componentsLoading,
-		error: componentsError
-	} = useGetDataAPI('http://127.0.0.1:8000/components/');
-	const {
-		data: partsData,
-		loading: partsLoading,
-		error: partsError
-	} = useGetDataAPI('http://127.0.0.1:8000/parts/');
-	const name = 'Inventory';
-	const { components, parts } = InventoryMockData();
+	const { toSentenceCase } = useContext(SentenceCaseContext)
+	// const currentPage = useLocation().pathname.split('/')[1];
+	// const refreshPage = useNavigate()
+	// const [refreshValue, setRefreshValue] = useState(false);
+	const { refreshIcon, handleRefresh } = useRefreshContext();
+	const { encrypt, RotCipher } = useContext(RotContext)
+	const [componentInventoryList, setComponentInventoryList] = useState(null);
+	const [partInventoryList, setParttInventoryList] = useState(null);
+	// const [refreshing, setRefreshing] = useState(false);
+	// const [isRefresh, setIsRefresh] = useState(false)
+	// const refreshChildComponent = () => {
+		
+	// }
+	// useEffect(() => {
+	// 	if (refreshValue) {
+	// 		handleRefreshAll()
+	// 		refreshPage('/success')
+	// 		const delay = setTimeout(() => {
+	// 			refreshPage(`/${currentPage}`)
+	// 			setRefreshValue(false)
+	// 		});
+	// 		return () => clearTimeout(delay);
+	// 	}
+	// }, [refreshValue])
 
+	// get setup
+	// inventoryComponents
+	const inventoryComponents = useGetWithEncryption(
+		'http://127.0.0.1:8000/components/',
+		'inventoryComponents',
+		// isRefresh,
+	)
+	useEffect(() => {
+		if (inventoryComponents) {
+			console.log('inventoryComponents with encryption: ', inventoryComponents)
+			if (inventoryComponents?.getData) {
+				// if (inventoryComponents.getData?.exist) {
+				// 	console.log(
+				// 		'inventoryComponents response:'.toUpperCase(),
+				// 		inventoryComponents.getData.exist
+				// 	)
+				// } else {
+					// creates and/or updates the localStorage
+					const data = inventoryComponents.getData
+					const encodedData = RotCipher(JSON.stringify(data), encrypt)
+					localStorage.setItem('inventoryComponents', encodedData)
+					setComponentInventoryList(data)
+					console.log('setting/updating'.toUpperCase())
+				// }
+			} else {
+				// gets updates from localStorage
+				setComponentInventoryList(inventoryComponents.localDataStoreVar)
+				console.log('retreiving'.toUpperCase())
+			}
+			// setComponentInventoryList(inventoryComponents.localDataStoreVar)
+		}
+	}, [inventoryComponents])
+	console.log('componentInventoryList:', componentInventoryList);
+
+	// inventoryParts
+	const inventoryParts = useGetWithEncryption(
+		'http://127.0.0.1:8000/parts/',
+		'inventoryParts',
+		// isRefresh,
+	)
+	useEffect(() => {
+		if (inventoryParts) {
+			console.log('inventoryParts with encryption: ', inventoryParts)
+			if (inventoryParts.getData) {
+				// creates and/or updates the localStorage
+				const data = inventoryParts.getData
+				const encodedData = RotCipher(JSON.stringify(data), encrypt)
+				localStorage.setItem('inventoryParts', encodedData)
+				setParttInventoryList(data)
+				console.log('setting/updating'.toUpperCase())
+				// handleRefresh(['inventoryComponents', 'inventoryParts'])
+			} else {
+				// gets updates from localStorage
+				setParttInventoryList(inventoryParts.localDataStoreVar)
+				console.log('retreiving'.toUpperCase())
+			}
+			// setComponentInventoryList(inventoryParts.localDataStoreVar)
+		}
+	}, [inventoryParts])
+	console.log('partInventoryList:', partInventoryList);
+	// refreshes the items
+	const refreshComponent = () => handleRefresh(['inventoryComponents', 'inventoryParts'])
 	const calculateStylesForComps = (value) => {
 		if (value === 0) {
 			return {
@@ -113,7 +190,7 @@ function Inventory() {
 		borderBottom: '2px solid',
 		textWrap: 'nowrap',
 	};
-	console.log('componentsData:', componentsData);
+	// console.log('componentsData:', componentsData);
 	const [addCompNames, setAddCompNames] = useState(false);
 	const [addPartNames, setAddPartNames] = useState(false);
 	const toggleAddCompNames = () => {
@@ -125,31 +202,23 @@ function Inventory() {
 	const activeStyles = {
 		backgroundColor: '#8A8A93',
 	}
+	const buttonStyle = {
+		paddingTop: '2rem',
+	}
+	// console.log('refreshvalue:', refreshValue)
 	return (
 		<div className="background-color custodian-page">
-			<CustomTime name={name} />
+			{/* <CustomTime name={name} /> */}
 			<div>
-				<h3>Inventory</h3>
-				<div className="custum-button">
-					<h5 style={addCompNames ? {...activeStyles} : {}}  onClick={toggleAddCompNames}>{addCompNames ? 'Close Inventory Component Names Form' : 'Add New Component Name'}</h5>
-					<h5 style={addPartNames ? {...activeStyles} : {}}  onClick={toggleAddPartNames}>{addPartNames ? 'Close Inventory Part Names Form' : 'Add New Part Name'}</h5>
-				</div>
-			</div>
-			<div>
-				{addCompNames && (
-					<>
-						{/* <hr /> */}
-						<AddComponentName />
-						{/* <LogFault childList={childList} /> */}
-					</>
-				)}
-				{addPartNames && (
-					<>
-						{/* <hr /> */}
-						<AddPartName />
-						{/* <LogFault childList={childList} /> */}
-					</>
-				)}
+				{/* <h3>Inventory</h3> */}
+				<h3 style={{display: 'flex', gap: '0.3rem'}}>
+					<span>
+						Inventory
+					</span>
+					<div onClick={refreshComponent}>
+						{refreshIcon}
+					</div>
+				</h3>
 			</div>
 			<div className="split-container">
 				<div className="inventory-list">
@@ -166,7 +235,8 @@ function Inventory() {
 							</tr>
 						</thead>
 						<tbody>
-						{componentsData && (componentsData.map((component, index) => {
+						{componentInventoryList &&
+						(componentInventoryList.map((component, index) => {
 							// const { backgroundColor, color, border, fontWeight, status } = calculateStylesForComps(component[1]);
 							const { color, status } = calculateStylesForComps(component.quantity);
 							return (
@@ -175,7 +245,7 @@ function Inventory() {
 										{index + 1}
 									</td>
 									<td style={staticStles}>
-										{component.name}
+										{toSentenceCase(component.name)}
 									</td>
 									<td style={staticStles}>
 										{component.quantity}
@@ -190,12 +260,26 @@ function Inventory() {
 									>
 										({status})
 									</td>
-									
 								</tr>
 							);
 						}))}
 						</tbody>
 					</table>
+					<div
+					style={buttonStyle}
+					className="custum-button">
+						<h5 style={addCompNames ? {...activeStyles} : {}}  onClick={toggleAddCompNames}>{addCompNames ? 'Close Form' : 'Add New Component Name'}</h5>
+					</div>
+					<div>
+						{addCompNames && (
+							<>
+								{/* <hr /> */}
+								<AddInventoryName inventoryName='component' />
+								{/* <AddComponentName /> */}
+								{/* <LogFault childList={childList} /> */}
+							</>
+						)}
+					</div>
 				</div>
 
 				<div
@@ -208,7 +292,8 @@ function Inventory() {
 				></div>
 
 				<div className="inventory-list">
-					<h4>Parts</h4>
+				<h4>Parts</h4>
+					{/* <h4>Parts</h4> */}
 					{/* <hr/> */}
 					<table>
 						<thead>
@@ -220,7 +305,7 @@ function Inventory() {
 							</tr>
 						</thead>
 						<tbody>
-						{partsData && (partsData.map((part, index) => {
+						{partInventoryList && (partInventoryList.map((part, index) => {
 							// const { backgroundColor, color, border, fontWeight, status } = calculateStylesForComps(component[1]);
 							const { color, status } = calculateStylesForParts(part.quantity);
 							return (
@@ -229,7 +314,7 @@ function Inventory() {
 										{index + 1}
 									</td>
 									<td style={staticStles}>
-										{part.name}
+										{toSentenceCase(part.name)}
 									</td>
 									<td style={staticStles}>
 										{part.quantity}
@@ -244,12 +329,26 @@ function Inventory() {
 									>
 										({status})
 									</td>
-									
 								</tr>
 							);
 						}))}
 						</tbody>
 					</table>
+					<div
+					style={buttonStyle}
+					className="custum-button">
+						<h5 style={addPartNames ? {...activeStyles} : {}}  onClick={toggleAddPartNames}>{addPartNames ? 'Close Form' : 'Add New Part Name'}</h5>
+					</div>
+					<div>
+						{addPartNames && (
+							<>
+								{/* <hr /> */}
+								<AddInventoryName inventoryName='part' />
+								{/* <AddPartName /> */}
+								{/* <LogFault childList={childList} /> */}
+							</>
+						)}
+					</div>
 				</div>
 			</div>
 		</div>
