93f6e48985399c4d85cdd0d498bd1b0344489ed5
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-09 21:45:49 +0100

    fixed SentenceCaseContext.js: fixed the component to process sentences and not just words into sentence case and added trim word function to it

--- altaviz/src/components/context/SentenceCaseContext.js
+++ altaviz/src/components/context/SentenceCaseContext.js
@@ -4,11 +4,31 @@ export const SentenceCaseContext = createContext();
 
 export const SentenceCaseProvider = ({ children }) => {
 	const toSentenceCase = (str) => {
-        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+        console.log('str:', str)
+        let sentenceArray;
+        if (str.includes(' ')) {
+            sentenceArray = str.split(' ');
+            console.log('sentenceArray:', sentenceArray)
+            sentenceArray = sentenceArray.map(word => word.charAt(0).toUpperCase()+word.slice(1).toLowerCase())
+            sentenceArray = sentenceArray.join(' ');
+        } else if (str.includes('-')) {
+            sentenceArray = str.split('-');
+            sentenceArray = sentenceArray.map(word => word.charAt(0).toUpperCase()+word.slice(1).toLowerCase())
+            sentenceArray = sentenceArray.join('-');
+        } else {
+            sentenceArray = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+        }
+        return sentenceArray;
     };
+    function trimString(str, maxLength) {
+        if (str.length > maxLength) {
+            return str.slice(0, maxLength - 3) + '...'; // Adjusting for the length of '...'
+        }
+        return str; // Return the original string if it's already shorter than maxLength
+    }
 
     return (
-        <SentenceCaseContext.Provider value={{toSentenceCase}}>
+        <SentenceCaseContext.Provider value={{toSentenceCase, trimString}}>
             {children}
         </SentenceCaseContext.Provider>
     );
