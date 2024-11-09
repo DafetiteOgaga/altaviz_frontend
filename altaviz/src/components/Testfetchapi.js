799c46aa8198f45820018987614e3c8089abbc34
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-09 21:31:55 +0100

    fixed Testfetchapi.js: Testfetchapi.js

--- altaviz/src/components/Testfetchapi.js
+++ altaviz/src/components/Testfetchapi.js
@@ -1,12 +1,18 @@
-import AddComponent from "./SideBar/componentComps/AddComponent";
-
+// import Deliveries from "./Testfdeliveries";
+// import { useContext } from "react";
+// import { AuthContext } from "./context/checkAuth/AuthContext";
+// import EngineerToLocation from "./SideBar/detailsUpdate/engineerToLocation";
 
 function Testfetchapi() {
+	// const { authData } = useContext(AuthContext);
+	// console.log('authData', authData);
 	return (
 		<>
 			<h2>Test Page</h2>
-			<AddComponent />
+			{/* <p>Test: {<EngineerToLocation />}</p> */}
+			<p>Test Page ends</p>
 		</>
 	);
 }
+
 export default Testfetchapi;
