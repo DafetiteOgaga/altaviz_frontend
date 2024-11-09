3ec5627956e10703aa0c6faa8f8f31936074e5e9
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-09 21:28:56 +0100

    fixed App.js: moved routes away to index.js and added sidebar component

--- altaviz/src/App.js
+++ altaviz/src/App.js
@@ -3,7 +3,7 @@
 import './App.css';
 import './components/fonts.css';
 // import Testfetchapi from './components/Testfetchapi';
-import SideBar from './components/SideBar/SideBar';
+import SideBar from './components/SideBar/sidebarCompoent/SideBar';
 import Header from './components/header/Header';
 import AppRoutes from './components/Routes/Routes';
 import Footer from './components/footer/Footer';
@@ -14,9 +14,7 @@ function App() {
   return (
     <div className="app-container">
       <div style={{paddingBottom: '1em'}}>
-        {/* <Router> */}
           <Header />
-          {/* <div className="content-wrapper"> */}
             <div className='body-minus-header'>
               <div className="sub-root">
                 <div>
@@ -30,64 +28,9 @@ function App() {
                 </main>
               </div>
             </div>
-          {/* </div> */}
-        {/* </Router> */}
       </div>
       <Footer />
     </div>
-
-
-
-    // <div className="app-container">
-    //   <div style={{paddingBottom: '1em'}}>
-    //     <Router>
-    //       <Header />
-    //       {/* <div className="content-wrapper"> */}
-    //         <div className='body-minus-header'>
-    //           <div className="sub-root">
-    //             <SideBar className="sidebar" />
-    //             <main>
-    //               <AppRoutes/>
-    //             </main>
-    //           </div>
-    //         </div>
-    //       {/* </div> */}
-    //     </Router>
-    //   </div>
-    //   <Footer />
-    // </div>
-
-
-
-
-
-    
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-		// 		<p>
-		// 			<a
-		// 				className="App-link"					
-    //       	href="https://github.com/DafetiteOgaga/custom_commands"					
-    //       	target="_blank"
-		// 				rel="noopener noreferrer"
-		// 			>
-    //       	Install more custom commands
-		// 			</a>
-		// 		</p>
-    //   </header>
-    // </div>
   );
 }
 
