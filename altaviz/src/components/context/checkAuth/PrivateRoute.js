de20a145c11ee82e40af1dae4dd5cfc0bcb738f3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-09 22:10:46 +0100

    fixed PrivateRoute.jsprovides or impedes the access to private routes depending on authentication state

--- altaviz/src/components/context/checkAuth/PrivateRoute.js
+++ altaviz/src/components/context/checkAuth/PrivateRoute.js
@@ -1,40 +1,21 @@
 import React, { useContext } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
 
 const PrivateRoute = () => {
 	console.log('querying AuthContext');
+    // const location = useLocation();
     const { isAuthenticated, loading } = useContext(AuthContext);
 	// Show a loading spinner or nothing while checking authentication
     if (loading) {
         return <div>Loading...</div>;
     }
+    // // If authenticated, but trying to access login page, redirect to /dashboard
+    // if (isAuthenticated && location.pathname === '/login') {
+    //     return <Navigate to="/dashboard" replace />;
+    // }
 	console.log('is authenticated from private route:', isAuthenticated);
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
-
-// // PrivateRoute.js
-// import React, { useContext } from 'react';
-// import { Route, Navigate } from 'react-router-dom';
-// import { AuthContext } from './AuthContext';
-
-// const PrivateRoute = ({ component: Component, ...rest }) => {
-//     const { isAuthenticated } = useContext(AuthContext);
-
-//     return (
-//         <Route
-//             {...rest}
-//             render={(props) =>
-//                 isAuthenticated ? (
-//                     <Component {...props} />
-//                 ) : (
-//                     <Navigate to="/login" replace />
-//                 )
-//             }
-//         />
-//     );
-// };
-
-// export default PrivateRoute;
