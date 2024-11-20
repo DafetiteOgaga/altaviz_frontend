6a49edd76d6b82e857fb3f99506359d7f6ff7527
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-11-20 19:00:52 +0100

    fixed AppProviders.js: removed unwanted global contexts, added websocket notification context and re-arranged the contexts accordingly

--- altaviz/src/components/context/AppProviders.js
+++ altaviz/src/components/context/AppProviders.js
@@ -3,12 +3,12 @@ import { GlobalProvider } from "./Context";
 import { LoginProvider } from "./loginAuth/LoginOutContext";
 import { AuthProvider } from "./checkAuth/AuthContext";
 import { SentenceCaseProvider } from "./SentenceCaseContext";
-import { SharedDataProvider } from "./sharedData/SharedDataContext";
+// import { SharedDataProvider } from "./sharedData/SharedDataContext";
 import { TimeDifferenceProvider } from "./timeDifference/TimeDifferenceContext";
-import { TriggerProvider } from "./triggerContext/TriggerContext";
+// import { TriggerProvider } from "./triggerContext/TriggerContext";
 import { RotProvider } from "./RotContext";
 import { RefreshProvider } from "./RefreshContext";
-import { RefreshComponentProvider } from "./refreshContext/TriggerRefresh";
+// import { RefreshComponentProvider } from "./refreshContext/TriggerRefresh";
 // import { SSENotificationProvider } from "./SSEContext/SSENotificationContext";
 import { WebSocketNotificationProvider } from "./RealTimeNotificationContext/useWebSocketNotificationContext";
 
