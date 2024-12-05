31edd6563b737d1ac53248e3026218b052386234
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-12-05 20:30:06 +0100

    fixed fetchFromLocalStorage.js: removed logs

--- altaviz/src/components/hooks/fetchFromLocalStorage.js
+++ altaviz/src/components/hooks/fetchFromLocalStorage.js
@@ -6,13 +6,13 @@ import { AuthContext } from "../context/checkAuth/AuthContext";
 function FetchFromLocalStorageOrDB(key, id=null) {
 	const { decrypt, RotCipher } = useContext(RotContext);
 	const storedData = localStorage.getItem(key);
-	console.log(`Fetching ${key} from local storage`);
+	// console.log(`Fetching ${key} from local storage`);
 	try {
-		console.log(`Fetching ${key} from local storage`);
+		// console.log(`Fetching ${key} from local storage`);
 		const decodedData = RotCipher(storedData, decrypt)
 		let parsed = JSON.parse(decodedData)
 		if (id) {parsed = parsed.filter(faults => faults.id === id)[0].faults}
-		console.log('parsed:', parsed);
+		// console.log('parsed:', parsed);
 		return parsed;
 	} catch (e) {
 		// console.log('Error:', e);
