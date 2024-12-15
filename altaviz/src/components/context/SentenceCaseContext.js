57e73ceef6eac411bf33cd0c3a0f23f2a6d42fe9
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-12-15 21:50:57 +0100

    fixed SentenceCaseContext.js: added the utility functions separateChars(), addHyphen() and removeHyphen() into the global contextAPI

--- altaviz/src/components/context/SentenceCaseContext.js
+++ altaviz/src/components/context/SentenceCaseContext.js
@@ -28,9 +28,33 @@ export const SentenceCaseProvider = ({ children }) => {
         }
         return str; // Return the original string if it's already shorter than maxLength
     }
+    const separateChars = (text) => {
+        console.log({text})
+        if (!text) return;
+        // Extract specific chunks using slice
+        const separated1 = text.slice(0, 3); // First 3 characters
+        const separated2 = text.slice(3, 7); // Next 4 characters
+        const separated3 = text.slice(7, 10); // Last 3 characters
+    
+        // Log the separated chunks
+        console.log('separated1:', separated1);
+        console.log('separated2:', separated2);
+        console.log('separated3:', separated3);
+    
+        // Combine the chunks with a space
+        return [separated1, separated2, separated3].filter(Boolean).join('-');
+    };
+
+    const addHyphen = (text) => {
+        return text.split(' ').join('-');
+    }
+
+    const removeHyphen = (text) => {
+        return text.split('-').join(' ');
+    }
 
     return (
-        <SentenceCaseContext.Provider value={{toSentenceCase, trimString}}>
+        <SentenceCaseContext.Provider value={{toSentenceCase, trimString, separateChars, addHyphen, removeHyphen}}>
             {children}
         </SentenceCaseContext.Provider>
     );
