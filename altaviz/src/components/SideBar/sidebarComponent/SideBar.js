bd8844e614a2cecd16a2b4d6b023631fce4d4671
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-12-18 15:20:55 +0100

    fixed SideBar.js: refactored the code to disable other departments except that of the current user, preventing foreign bodies from visiting or accessing departments they are alien to. non members can equally not access any department unless they are signed in

--- altaviz/src/components/SideBar/sidebarComponent/SideBar.js
+++ altaviz/src/components/SideBar/sidebarComponent/SideBar.js
@@ -22,25 +22,38 @@ function Sidebar() {
             console.error('Logout failed');
         }
     };
+	const dept = authData?.role
+	const deptStyles = {
+		color: '#999',
+		pointerEvents: 'none',
+		cursor: 'default'
+	}
+	console.log({dept})
+	const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+	console.log('\napiBaseUrl url:', apiBaseUrl)
 	return (
 		<aside className='side-bar'>
 			<nav>
 				<h2>Sections</h2>
 				<ul className='sidebar-ul'>
 					{/* <Link to="/"><li>Home</li></Link> */}
-					<Link to="/custodian"><li>Custodian</li></Link>
-					<Link to="/workshop"><li>Workshop</li></Link>
-					<Link to="/engineer"><li>Engineer</li></Link>
-					<Link to="/help-desk"><li>Help Desk</li></Link>
-					<Link to="/supervisor"><li>Supervisor</li></Link>
-					<Link to="/human-resource"><li>Human Resource</li></Link>
+					<Link style={dept==='custodian'?null:deptStyles} to={`/${dept}`}><li>Custodian</li></Link>
+					<Link style={dept==='workshop'?null:deptStyles} to={`/${dept}`}><li>Workshop</li></Link>
+					<Link style={dept==='engineer'?null:deptStyles} to={`/${dept}`}><li>Engineer</li></Link>
+					<Link style={dept==='help-desk'?null:deptStyles} to={`/${dept}`}><li>Help Desk</li></Link>
+					<Link style={dept==='supervisor'?null:deptStyles} to={`/${dept}`}><li>Supervisor</li></Link>
+					<Link style={dept==='human-resource'?null:deptStyles} to={`/${dept}`}><li>Human Resource</li></Link>
 					{!isAuthenticated ?
-					(<Link to="/login"><li>Login</li></Link>) :
-					(<Link onClick={logHandler} to="/"><li>Logout</li></Link>)}
+						(<Link to="/login"><li>Login</li></Link>) :
+						(<Link onClick={logHandler} to="/"><li>Logout</li></Link>)
+					}
 					<Link to="/profile"><li>Profile</li></Link>
-					{/* <Link to="/profile"><li>Profile</li></Link> */}
-					<Link to="/test-auth"><li>test authentication</li></Link>
-					<Link to="/test"><li>testing backend</li></Link>
+					{
+					apiBaseUrl==='http://127.0.0.1:8000' &&
+					<>
+						<Link to="/test-auth"><li>test authentication</li></Link>
+						<Link to="/test"><li>testing backend</li></Link>
+					</>}
 				</ul>
 			</nav>
 		</aside>
