e630ccff8c853d23a909a5bd4dc8ef0ca60adfa4
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-01-07 01:46:48 +0100

    new BackgroundUpdate.js: the function that streamline and handles background updates

---
+++ altaviz/src/components/context/BackgroundUpdates/BackgroundUpdate.js
@@ -0,0 +1,51 @@
+import { RotContext } from "../RotContext";
+import { useContext } from "react";
+
+const useBackgroundUpdate = async (endpoints, action=false) => {
+	const {encrypt, RotCipher} = useContext(RotContext);
+	if (!action) return;
+	try {
+		// Fetch data from all endpoints concurrently using Promise.all
+		const responses = await Promise.all(
+		endpoints.map((arr) => {
+			console.log('arr:'.repeat(25), arr)
+			const [key, endpoint] = arr.split('-/');
+			console.log(
+				'\nkey:'.repeat(30), key,
+				'\nendpoint:'.repeat(30), endpoint,
+			)
+			return (
+				fetch(endpoint).then(async (res) => {
+				if (!res.ok) {
+					throw new Error(`Failed to fetch ${endpoint}: ${res.statusText}`);
+				}
+				const data = await res.json();
+				// const localData = JSON.stringify(data)
+				const localData = RotCipher(JSON.stringify(data), encrypt)
+				console.log('localData:'.repeat(10), key, ': =>', localData.slice(0, 60))
+				localStorage.setItem(key, localData)
+				const getCurrUpdateValue = localStorage.getItem('updateCounter')
+				if (getCurrUpdateValue) {
+					const [currNum, totalNum] = getCurrUpdateValue.split('/')
+					if (currNum!==totalNum) {
+						localStorage.setItem('updateCounter', Number(currNum)+1+'/'+totalNum)
+					} else {
+						localStorage.removeItem('updateCounter')
+					}
+				} else {
+					localStorage.setItem('updateCounter', 1+'/'+endpoints.length)
+				}
+				return data;
+
+				})
+			)
+		}));
+		console.log('all endpoints fetched and stored successfully'.repeat(30))
+		// localStorage.setItem('updatedItem', 'inventoryComponents')
+		return responses; // Array of responses, one for each endpoint
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		throw error;
+	}
+};
+export { useBackgroundUpdate };
