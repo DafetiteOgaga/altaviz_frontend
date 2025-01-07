82a5216e7693a7f99835bc39508ab0f5fb0f355b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-01-07 01:51:11 +0100

    fixed Success.js: fixed redirection time

--- altaviz/src/components/success/Success.js
+++ altaviz/src/components/success/Success.js
@@ -1,17 +1,20 @@
 // import { useLocation} from 'react';
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 import './success.css';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../context/checkAuth/AuthContext';
 // import { useNavigate } from 'react-router-dom';
 
 const Success = () => {
+  const { authData } = useContext(AuthContext);
   const navigate = useNavigate()
   const location = useLocation();
   const { currentPage, time } = location.state||{};
   useEffect(()=>{
     const delay = setTimeout(() => {
       if (currentPage) navigate(currentPage)
-    }, time||1000);
+      else navigate(`/${authData.role}`||'/')
+    }, currentPage?(time||1000):0);
     return () => clearTimeout(delay);
   }, [currentPage])
   console.log({location})
