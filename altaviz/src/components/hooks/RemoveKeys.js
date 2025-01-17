a935bf8c4b411d04d4da3c71cf68eac87a978016
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-01-17 18:57:52 +0100

    fixed RemoveKeys.js: added localstorageList(), RemoveAllKeys(), and RemoveAllKeysButAuth() utility functions

--- altaviz/src/components/hooks/RemoveKeys.js
+++ altaviz/src/components/hooks/RemoveKeys.js
@@ -26,13 +26,6 @@ export function RemoveAllKeys () {
 export function RemoveAllKeysButAuth () {
 	let localList = localstorageList()
 	localList = localList.filter(key => key !== 'authData')
-	// let localList = [] // gather local keys
-	// for (let i = 0; i < localStorage.length; i++) {
-	// 	if (localStorage.key(i) === 'authData') continue // exempt authentication date
-	// 	console.log('appending localkey:'.toUpperCase(), localStorage.key(i));
-	// 	localList.push(localStorage.key(i));
-	// 	// localStorage.removeItem(localStorage.key(i));
-	// }
 	console.log(localList.map(key => `key: ${key}`))
 	for (const key of localList) {
 		localStorage.removeItem(key);  // Remove local keys and their values from localStorage
