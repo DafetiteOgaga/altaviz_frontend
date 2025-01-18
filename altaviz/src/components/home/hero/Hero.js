4c799a4b430e6e85bc32568f413ba4644c338790
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-01-18 18:56:15 +0100

    fixed Hero.js: added box shadow to the hero display

--- altaviz/src/components/home/hero/Hero.js
+++ altaviz/src/components/home/hero/Hero.js
@@ -28,6 +28,7 @@ function Hero() {
 		className="hero"
 		style={{
 			backgroundImage: `url(${images[currentImageIndex]})`,
+			boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
 		}}
 		>
 		<div className="hero-content">
