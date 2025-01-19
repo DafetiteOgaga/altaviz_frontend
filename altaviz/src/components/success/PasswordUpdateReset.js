863cfe2dbc1bbbd2e819e72e6d6f9aa48bcdf65a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-01-19 16:15:50 +0100

    fixed PasswordUpdateReset.js: switched styling from inline to styled so that i can use keyframes without css file

--- altaviz/src/components/success/PasswordUpdateReset.js
+++ altaviz/src/components/success/PasswordUpdateReset.js
@@ -1,6 +1,40 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
 
+const fadeInSuccess = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+const slideInSucces = keyframes`
+  from {
+    transform: translateY(-50px);
+  }
+  to {
+    transform: translateY(0);
+  }
+`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+  padding: 1rem 8rem;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  animation: ${fadeInSuccess} 1.7s ease-in-out;
+`
+const Head1 = styled.h1`
+  text-align: center;
+  margin-bottom: 20px;
+  animation: ${slideInSucces} 0.5s ease-out;
+  `
 const PasswordUpdateReset = () => {
   // const { authData } = useContext(AuthContext);
   const navigate = useNavigate()
@@ -16,12 +50,12 @@ const PasswordUpdateReset = () => {
   console.log({location})
   return (
     <>
-      <styles>{globalStyles}</styles>
+      {/* <styles>{globalStyles}</styles> */}
       <div style={styles.body}>
-        <div style={styles.container}>
-          <h1 style={styles.head1}>Success!</h1>
+        <Container>
+          <Head1 style={{color: (type==='error')?'red':'rgba(0, 0, 0, 0.575)',}}>{(type==='error')?'Oopsy':'Success'}!</Head1>
           <h2 style={styles.head1}>{text}</h2>
-        </div>
+        </Container>
       </div>
     </>
   );
@@ -35,43 +69,9 @@ const styles = {
     alignItems: 'center',
     paddingTop: '8rem',
   },
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-    flexWrap: 'wrap',
-    padding: '1rem 8rem',
-    background: '#fff',
-    borderRadius: '10px',
-    boxShadow: '0 0 20px rgba(0, 0, 0, 0.1)',
-    animation: 'fadeInSuccess 1.7s ease-in-out',
-  },
   head1: {
-    // fontSize: '2.5rem',
     textAlign: 'center',
-    color: 'rgba(0, 0, 0, 0.575)',
     marginBottom: '20px',
     animation: 'slideInSucces 0.5s ease-out',
   }
 }
-
-const globalStyles = () =>`
-  @keyframes fadeInSuccess {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-
-  @keyframes slideInSucces {
-    from {
-      transform: translateY(-50px);
-    }
-    to {
-      transform: translateY(0);
-    }
-  }
-`;
